221f00001b98d2e2f11d391dbbff1bdf55adc97e
Neil C. Obremski <masta@gibdon.com>
2020-12-21 11:59:03 -0800

    Add listener for button, change to linear layout

--- src/life/nosk/motator/MainActivity.kt
+++ src/life/nosk/motator/MainActivity.kt
@@ -1,17 +1,17 @@
 package life.nosk.motator
 
-/*
-fun main() {
-    println("Hello world! 0x" + R.layout.activity_main.toString(16))
-}
-*/
-
 import android.app.Activity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState:Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val myButton = findViewById(R.id.my_button) as Button
+        myButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
+        }
     }
 }
