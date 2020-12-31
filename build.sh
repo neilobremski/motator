9f18e8784491be98eacac2ee2b7872177cea38cc
Neil C. Obremski <masta@gibdon.com>
2020-12-30 16:46:41 -0800

    Add OSMdroid to build script (put in ./aar/ dir)

--- build.sh
+++ build.sh
@@ -18,23 +18,33 @@ rm -rf $MOTATOR/bin
 mkdir -p $MOTATOR/bin
 rm -rf $MOTATOR/obj
 mkdir -p $MOTATOR/obj
+rm -rf $MOTATOR/build_files
+mkdir -p $MOTATOR/build_files
+
+echo "Extracting AAR's"
+unzip aar/osmdroid-android-6.1.8-release.aar -d build_files/
 
 # TODO: switch to aapt2
 # TODO: res/drawable/ 
 echo "Building R.java (resources file)"
-$BUILD_TOOLS/aapt package -f -m -J src -M $MOTATOR/AndroidManifest.xml -S $MOTATOR/res -I $ANDROID_JAR
+$BUILD_TOOLS/aapt package --auto-add-overlay -f -m -J src -M $MOTATOR/build_files/AndroidManifest.xml -S $MOTATOR/res -S $MOTATOR/build_files/res -I $ANDROID_JAR
+$BUILD_TOOLS/aapt package --auto-add-overlay -f -m -J src -M $MOTATOR/AndroidManifest.xml -S $MOTATOR/res -S $MOTATOR/build_files/res -I $ANDROID_JAR
 
 echo "Compiling Java Code"
-javac -source 1.7 -target 1.8 -d $MOTATOR/obj -classpath $MOTATOR/src -bootclasspath $ANDROID_JAR $MOTATOR/src/life/nosk/motator/*.java
+javac -source 1.7 -target 1.8 -d $MOTATOR/obj -classpath $MOTATOR/src:$MOTATOR/build_files/classes.jar -bootclasspath $ANDROID_JAR $MOTATOR/src/life/nosk/motator/*.java
+javac -source 1.7 -target 1.8 -d $MOTATOR/obj -classpath $MOTATOR/src:$MOTATOR/build_files/classes.jar -bootclasspath $ANDROID_JAR $MOTATOR/src/org/osmdroid/library/*.java
 
 echo "Compiling Kotlin Code"
-kotlinc -jvm-target 1.8 -include-runtime -d $MOTATOR/obj/k.jar -classpath $MOTATOR/obj:$ANDROID_JAR $MOTATOR/src/life/nosk/motator/*.kt
+kotlinc -jvm-target 1.8 -include-runtime -d $MOTATOR/obj/k.jar -classpath $MOTATOR/obj:$ANDROID_JAR::$MOTATOR/build_files/classes.jar $MOTATOR/src/life/nosk/motator/*.kt
 
 echo "Extracting Compiled Classes"
+cp $MOTATOR/build_files/classes.jar $MOTATOR/obj
 pushd $MOTATOR/obj
 jar xf k.jar
+jar xf classes.jar
 popd
 rm $MOTATOR/obj/k.jar
+rm $MOTATOR/obj/classes.jar
 # fixes the following:
 # PARSE ERROR:
 # unsupported class file version 53.0
@@ -46,7 +56,7 @@ echo "Converting to Dalvik"
 $BUILD_TOOLS/dx --dex --output=$MOTATOR/bin/classes.dex $MOTATOR/obj
 
 echo "Creating APK (unaligned)"
-aapt package -f -m -F $MOTATOR/bin/motator.unaligned.apk -M $MOTATOR/AndroidManifest.xml -S $MOTATOR/res -I $ANDROID_JAR
+aapt package --auto-add-overlay -f -m -F $MOTATOR/bin/motator.unaligned.apk -M $MOTATOR/AndroidManifest.xml -S $MOTATOR/res -S $MOTATOR/build_files/res -I $ANDROID_JAR
 
 echo "Adding Code to APK"
 pushd $MOTATOR/bin
