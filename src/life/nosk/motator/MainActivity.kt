1006f3ca8f955c9456f355a922bb4ac05a1a7ae0
Neil C. Obremski <masta@gibdon.com>
2021-01-11 14:46:39 -0800

    Support screen rotation

--- src/life/nosk/motator/MainActivity.kt
+++ src/life/nosk/motator/MainActivity.kt
@@ -5,8 +5,10 @@ import android.app.Activity
 import android.content.Intent
 
 import android.os.Bundle
+import android.os.Build
 import android.widget.Button
 import android.widget.Toast
+import android.widget.LinearLayout
 import android.Manifest
 import android.content.pm.PackageManager
 import android.content.Context
@@ -18,6 +20,7 @@ import android.widget.TextView
 import android.widget.SeekBar
 import android.preference.PreferenceManager
 import android.location.Criteria
+import android.view.ViewGroup
 
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
@@ -68,7 +71,12 @@ class MainActivity : Activity() {
                             it.setPosition(GeoPoint(latestLocation))
                             it.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
                             map!!.getOverlays().add(startMarker)
-                            it.setIcon(getResources().getDrawable(R.drawable.ic_menu_mylocation))
+                            if (Build.VERSION.SDK_INT < 21) {
+                                @Suppress("DEPRECATION")
+                                it.setIcon(getResources().getDrawable(R.drawable.ic_menu_mylocation))
+                            } else {
+                                it.setIcon(getResources().getDrawable(R.drawable.ic_menu_mylocation, null))
+                            }
                             it.title = "Start point"
                         }
                     }
@@ -125,6 +133,32 @@ class MainActivity : Activity() {
                 }
             }
         }
+    }
+
+    override fun onConfigurationChanged(newConfig: android.content.res.Configuration) {
+        Log.i("MOTATOR", "Configuration changed")
+        super.onConfigurationChanged(newConfig)
+
+        val linearLayout = findViewById(R.id.main_layout) as LinearLayout
+        var lp = LinearLayout.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, 0, 0.5f)
+
+        // Checks the orientation of the screen
+        if (newConfig.orientation == android.content.res.Configuration.ORIENTATION_LANDSCAPE) {
+            Log.i("MOTATOR", "Orientation: Landscape")
+            linearLayout.orientation = LinearLayout.HORIZONTAL
+            lp = LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.FILL_PARENT, 0.5f)
+
+        } else if (newConfig.orientation == android.content.res.Configuration.ORIENTATION_PORTRAIT) {
+            Log.i("MOTATOR", "Orientation: Portrait")
+            linearLayout.orientation = LinearLayout.VERTICAL
+        }
+
+        for (i in 0..linearLayout.getChildCount()-1) {
+            val child = linearLayout.getChildAt(i)
+            child.setLayoutParams(lp)
+        }
+
+        linearLayout.invalidate()
     }
 
     override fun onRequestPermissionsResult(requestCode: Int,
--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-adb uninstall life.nosk.motator
-adb install bin/motator.apk
-adb shell am start -n life.nosk.motator/life.nosk.motator.MainActivity
+adb $ADB_ARGS uninstall life.nosk.motator
+adb $ADB_ARGS install bin/motator.apk
+adb $ADB_ARGS shell am start -n life.nosk.motator/life.nosk.motator.MainActivity
