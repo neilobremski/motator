99fb1d3c5097c02c1b45aacb63a8bb68829de417
Neil C. Obremski <masta@gibdon.com>
2021-01-11 14:47:18 -0800

    Added ADB_ARGS explanation

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
+
+# define ADB_ARGS="-s <device ID>" to have this work w/ specific device
 adb $ADB_ARGS uninstall life.nosk.motator
 adb $ADB_ARGS install bin/motator.apk
 adb $ADB_ARGS shell am start -n life.nosk.motator/life.nosk.motator.MainActivity
